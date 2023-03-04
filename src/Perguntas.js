6d1cf51c02f7677a954aed428e0eb773a387f38f
LucasBento <lubentoneves@gmail.com>
2023-03-04 17:16:59 -0300

    flashcards finalizados

--- src/Perguntas.js
+++ src/Perguntas.js
@@ -1,33 +1,66 @@
+import { useState } from "react"
 import styled from "styled-components"
 import seta from "./assets/seta_play.png"
 import virar from "./assets/seta_virar.png"
+import iconeCerto from "./assets/icone_certo.png"
+import iconeErrado from "./assets/icone_erro.png"
+import iconeQuase from  "./assets/icone_quase.png"
 
 export default function Perguntas({pergunta, resposta , index}) {
+    const [cardAtual, setCardAtual] = useState(1)
+    const [respostas, setRespostas] = useState('')
+
+    function proximaEtapa(){
+        const proximo = cardAtual + 1 ;
+        setCardAtual(proximo)
+    }
+
+    function errou(){
+        setCardAtual(1);
+        setRespostas('#FF3030')
+    }
+
+    function acertou(){
+        setCardAtual(1);
+        setRespostas('#2FBE34')
+    }
+
+    function porpouco(){
+        setCardAtual(1);
+        setRespostas('#FF922E')
+    }
+    const reavaliar2 = [respostas==="#2FBE34" ? <img src={iconeCerto} alt="Acertou" /> : <img src={iconeQuase} alt="Seguir" />]
+
+
+    const reavaliar = [respostas==="#FF3030" ? <img src={iconeErrado} alt="Errou" /> : reavaliar2]
+   
+    const ternarioImagem = [respostas==="" ? <img onClick={proximaEtapa} src={seta} alt="Seguir" /> : reavaliar]
+   
     return (
-        <Questions>
+        <Questions cardAtual={cardAtual} respostas={respostas}>
             <div className="carta">
                 <div className="face1">
                     <p>Pergunta {index + 1}</p>
-                    <img src={seta} alt="Seguir" />
+                    {ternarioImagem}
                 </div>
 
                 <div className="face2">
                     <p>{pergunta}</p>
-                    <img src={virar} alt="Seguir" />
+                    <img onClick={proximaEtapa} src={virar} alt="Seguir" />
                 </div>
 
                 <div className="face3">
                     <p>{resposta}</p>
                     <div className="botoes">
-                        <button className="falha">
+                        <button onClick={errou} className="falha">
                             Não lembrei
                         </button>
 
-                        <button className="quase">
+                        <button onClick={porpouco} className="quase">
                             Quase não lembrei
                         </button>
 
-                        <button className="acerto">
+                        <button onClick={acertou} className="acerto">
                             Zap!
                         </button>
                     </div>
@@ -44,13 +77,17 @@ const Questions = styled.div`
     justify-content: center;
     align-items: center;
 
+
     .carta{
-        margin-bottom: 25px;
+        margin-bottom: ${ props => props.cardAtual===3 ? '85px' : '25px'};
         position: relative;
+        background-color: blue;
     }
 
 
     .face1{
+        position: absolute;
+        top: 0px;
         width:300px;
         height:65px ;
         border-radius:5px;
@@ -62,56 +99,76 @@ const Questions = styled.div`
         padding-left: 10px;
         padding-right: 10px;
         box-sizing: border-box;
+        visibility: ${ props => props.cardAtual===1 ? "" : 'hidden'};
 
         p{
             font-family: 'Recursive', sans-serif;
             font-weight: 700;
             font-size: 16px;
             line-height: 19px;
-            color:#333333;
+            color: ${ prop => prop.respostas === '' ? '#333333' : prop.respostas};
+            text-decoration: ${ prop => prop.respostas === '' ? '#333333' : "line-through"};
         };
 
         img{
             width: 20px ;
-            height: 23px ;
+            height: 20px ;
         }
     }
 
     .face2{
+        position: absolute;
+        top: 0px;
         width:300px;
-        min-height: 130px;
+        min-height: ${ props => props.cardAtual===2 ? '130px' : '65px'};
         border-radius:5px;
         background-color:#FFFFD4;
         box-shadow: 0px 4px 5px 0px #00000026;
         position: relative;
+        visibility: ${ props => props.cardAtual===2 ? "" : 'hidden'};
+        box-sizing: border-box;
+        
         
         p{
             padding-top:20px;
             padding-left: 15px;
+            padding-right: 15px;
             padding-bottom: 35px;
+            font-family: 'Recursive', sans-serif;
+            font-weight: 400;
+            font-size: ${ props => props.cardAtual===2 ? "18px" : '0px'};
+            line-height: 21px;
+            color:#333333;
         };
 
         img{
             position: absolute;
             bottom: 6px;
             right: 15px;
-            width: 30px;
-            height: 20px;
+
         }
     }
 
     .face3{
+        position: absolute;
+        top: 0px;
         width:300px;
-        min-height: 130px;
+        min-height: ${ props => props.cardAtual===3 ? '130px' : '65px'};
         border-radius:5px;
         background-color:#FFFFD4;
         box-shadow: 0px 4px 5px 0px #00000026;
-        position: relative;
+        visibility: ${ props => props.cardAtual===3 ? "" : 'hidden'};
 
         p{
             padding-top:20px;
             padding-left: 15px;
-            padding-bottom: 35px;
+            padding-right: 15px;
+            padding-bottom: 60px;
+            font-family: 'Recursive', sans-serif;
+            font-weight: 400;
+            font-size: 18px;
+            line-height: 21px;
+            color:#333333;
         }
 
         div{
